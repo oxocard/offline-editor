c2de8305b27156b1be16c2394c7daa3e3b35f46b
ST3tter <st3tter@gmail.com>
2024-06-10 12:30:59 +0200

    Added denounce timeout for the run and debug run command

--- src/components/ToolsHeader/index.tsx
+++ src/components/ToolsHeader/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import styled, { ThemeContext } from 'styled-components';
 import { batch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
@@ -7,6 +7,7 @@ import Serial from '../../serial';
 /* Components */
 import SvgButton from '../SvgButton';
 import ObserverSlider from './ObserverSlider';
+import CircularProgress from '@mui/material/CircularProgress';
 
 /* Store */
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
@@ -33,12 +34,32 @@ export default function ToolsHeader() {
   const { t } = useTranslation();
   const theme = useContext(ThemeContext);
 
+  const [runDebounce, setRunDebounce] = useState(false);
+
   const editorMode = useAppSelector((state) => state.editor.mode);
   const breakpointLine = useAppSelector((state) => state.editor.breakpointLine);
   const observeSpeed = useAppSelector((state) => state.editor.observeSpeed);
   const isConnected = useAppSelector((state) => state.device.isConnected);
 
   const serial = Serial.getInstance();
+
+  /* Debounce the run and rundebug commands because the card can not handle
+     multiple calls before the last one finished  */
+  const startRunDebounce = () => {
+    setRunDebounce(true);
+    setTimeout(() => {
+      setRunDebounce(false);
+    }, 2000);
+  };
+
+  /* Cancel the debounce if the card is ready */
+  useEffect(() => {
+    if (editorMode >= editorModes.run) {
+      if (runDebounce) {
+        setRunDebounce(false);
+      }
+    }
+  }, [editorMode, runDebounce]);
 
   return (
     <ToolsHeaderContainer id="tool_bar">
@@ -47,8 +68,9 @@ export default function ToolsHeader() {
         altText="Run code"
         active={editorMode === editorModes.run}
         discreet={editorMode === editorModes.debug}
-        disabled={!isConnected}
+        disabled={!isConnected || runDebounce}
         onClick={() => {
+          startRunDebounce();
           batch(() => {
             dispatch(resetError());
             dispatch(setShowDeviceState(true));
@@ -56,24 +78,29 @@ export default function ToolsHeader() {
           serial.sendCode();
         }}
       >
-        <svg
-          className="svg_icon"
-          id="svg_run_code"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 60 60"
-          data-tooltip-id="main-tooltip"
-          data-tooltip-content={t('tools_header_btn_run')}
-        >
-          <path d="M33.32,27.73h4.23c1.1,0,1.41.68.69,1.52l-9,10.44c-1.08,1.26-1.95.94-1.95-.72V32.73H23.08c-1.1,0-1.41-.68-.69-1.52l9-10.44c1.08-1.26,2-.94,2,.72Zm-2-3.85-5.89,6.85h2.88a1,1,0,0,1,1,1v4.85l5.89-6.85H32.32a1,1,0,0,1-1-1Z" />
-        </svg>
+        {runDebounce ? (
+          <CircularProgress size={'2rem'} color="success" />
+        ) : (
+          <svg
+            className="svg_icon"
+            id="svg_run_code"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 60 60"
+            data-tooltip-id="main-tooltip"
+            data-tooltip-content={t('tools_header_btn_run')}
+          >
+            <path d="M33.32,27.73h4.23c1.1,0,1.41.68.69,1.52l-9,10.44c-1.08,1.26-1.95.94-1.95-.72V32.73H23.08c-1.1,0-1.41-.68-.69-1.52l9-10.44c1.08-1.26,2-.94,2,.72Zm-2-3.85-5.89,6.85h2.88a1,1,0,0,1,1,1v4.85l5.89-6.85H32.32a1,1,0,0,1-1-1Z" />
+          </svg>
+        )}
       </SvgButton>
       <SvgButton
         id="btn_debug_code"
         altText="Debug code"
         active={editorMode >= editorModes.debug}
         discreet={editorMode === editorModes.run}
-        disabled={!isConnected}
+        disabled={!isConnected || runDebounce}
         onClick={() => {
+          startRunDebounce();
           batch(() => {
             dispatch(resetError());
             dispatch(setShowDeviceState(true));
@@ -81,16 +108,20 @@ export default function ToolsHeader() {
           serial.sendDebugCode();
         }}
       >
-        <svg
-          className="svg_icon"
-          id="svg_debug_code"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 60 60"
-          data-tooltip-id="main-tooltip"
-          data-tooltip-content={t('tools_header_btn_debug')}
-        >
-          <path d="M26,30.1c0.8,0.3,1.2,1.1,0.9,1.9c-0.2,0.6-0.8,1-1.4,1c-0.2,0-0.3,0-0.5,0c0,0.6,0.1,1.1,0.3,1.7c0.2-0.1,0.5-0.2,0.7-0.2	c0.8,0,1.5,0.7,1.5,1.5c0,0.4-0.2,0.8-0.4,1c0.6,0.4,1.2,0.7,1.9,0.8v-3.2c-1-0.6-1.3-1.8-0.7-2.7c0.2-0.3,0.4-0.6,0.7-0.7v-1.8	c-0.6-0.1-1.2-0.3-1.7-0.5h-0.1C26.7,29.2,26.3,29.6,26,30.1z M32,27c0-1.1-0.9-2-2-2c-1.1,0-2,0.9-2,2c0,0,0,0,0,0.1h0.1	C29.3,27.6,30.7,27.6,32,27L32,27C32,27,32,27,32,27z M32.8,28.9c-0.6,0.3-1.2,0.4-1.8,0.5v1.8c1,0.6,1.3,1.8,0.7,2.7	c-0.2,0.3-0.4,0.6-0.7,0.7v3.2c0.7-0.1,1.4-0.4,1.9-0.8c-0.3-0.3-0.4-0.7-0.4-1.1c0-0.8,0.7-1.5,1.5-1.5c0.2,0,0.5,0.1,0.7,0.2 c0.2-0.5,0.3-1.1,0.3-1.7c-0.2,0-0.3,0-0.5,0c-0.8,0-1.5-0.7-1.5-1.5c0-0.7,0.4-1.2,1-1.4C33.7,29.6,33.3,29.2,32.8,28.9L32.8,28.9z M31.1,22c0.8-1.3,2.3-2.1,3.9-2c0.6,0,1,0.4,1,1s-0.4,1-1,1c-0.9-0.1-1.7,0.3-2.2,1c-0.1,0.2-0.2,0.5-0.3,0.7c0,0,0,0.1,0,0.1 c1,0.8,1.5,2.1,1.5,3.4c0.3,0.2,0.5,0.4,0.8,0.6l1.2-1.3c0.4-0.4,1.1-0.3,1.4,0.1c0.3,0.4,0.3,0.9,0,1.3L36,29.4	c0.5,0.8,0.8,1.7,0.9,2.6H39c0.6,0,1,0.4,1,1s-0.4,1-1,1h-2.1c-0.1,0.9-0.4,1.8-0.9,2.6l1.4,1.4c0.4,0.4,0.4,1,0,1.4 c-0.4,0.4-1,0.4-1.4,0l-1.3-1.3c-2.7,2.5-6.8,2.5-9.5,0L24,39.4c-0.4,0.4-1,0.4-1.4,0c-0.4-0.4-0.4-1,0-1.4l1.4-1.4 c-0.5-0.8-0.8-1.7-0.9-2.6H21c-0.6,0-1-0.4-1-1s0.4-1,1-1h2.1c0.1-0.9,0.4-1.8,0.9-2.6L22.6,28c-0.4-0.4-0.3-1.1,0.1-1.4	c0.4-0.3,0.9-0.3,1.3,0l1.3,1.3c0.2-0.2,0.5-0.4,0.8-0.6c-0.1-1.3,0.5-2.6,1.5-3.4c0,0,0-0.1,0-0.1c-0.2-1.2-1.3-1.9-2.4-1.7 c0,0-0.1,0-0.1,0c-0.6,0-1-0.4-1-1s0.4-1,1-1c1.6-0.1,3.1,0.6,3.9,2c0.2,0.3,0.3,0.7,0.4,1c0.2,0,0.4,0,0.6,0c0.2,0,0.4,0,0.7,0 C30.8,22.6,30.9,22.3,31.1,22z" />
-        </svg>
+        {runDebounce ? (
+          <CircularProgress size={'2rem'} color="success" />
+        ) : (
+          <svg
+            className="svg_icon"
+            id="svg_debug_code"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 60 60"
+            data-tooltip-id="main-tooltip"
+            data-tooltip-content={t('tools_header_btn_debug')}
+          >
+            <path d="M26,30.1c0.8,0.3,1.2,1.1,0.9,1.9c-0.2,0.6-0.8,1-1.4,1c-0.2,0-0.3,0-0.5,0c0,0.6,0.1,1.1,0.3,1.7c0.2-0.1,0.5-0.2,0.7-0.2	c0.8,0,1.5,0.7,1.5,1.5c0,0.4-0.2,0.8-0.4,1c0.6,0.4,1.2,0.7,1.9,0.8v-3.2c-1-0.6-1.3-1.8-0.7-2.7c0.2-0.3,0.4-0.6,0.7-0.7v-1.8	c-0.6-0.1-1.2-0.3-1.7-0.5h-0.1C26.7,29.2,26.3,29.6,26,30.1z M32,27c0-1.1-0.9-2-2-2c-1.1,0-2,0.9-2,2c0,0,0,0,0,0.1h0.1	C29.3,27.6,30.7,27.6,32,27L32,27C32,27,32,27,32,27z M32.8,28.9c-0.6,0.3-1.2,0.4-1.8,0.5v1.8c1,0.6,1.3,1.8,0.7,2.7	c-0.2,0.3-0.4,0.6-0.7,0.7v3.2c0.7-0.1,1.4-0.4,1.9-0.8c-0.3-0.3-0.4-0.7-0.4-1.1c0-0.8,0.7-1.5,1.5-1.5c0.2,0,0.5,0.1,0.7,0.2 c0.2-0.5,0.3-1.1,0.3-1.7c-0.2,0-0.3,0-0.5,0c-0.8,0-1.5-0.7-1.5-1.5c0-0.7,0.4-1.2,1-1.4C33.7,29.6,33.3,29.2,32.8,28.9L32.8,28.9z M31.1,22c0.8-1.3,2.3-2.1,3.9-2c0.6,0,1,0.4,1,1s-0.4,1-1,1c-0.9-0.1-1.7,0.3-2.2,1c-0.1,0.2-0.2,0.5-0.3,0.7c0,0,0,0.1,0,0.1 c1,0.8,1.5,2.1,1.5,3.4c0.3,0.2,0.5,0.4,0.8,0.6l1.2-1.3c0.4-0.4,1.1-0.3,1.4,0.1c0.3,0.4,0.3,0.9,0,1.3L36,29.4	c0.5,0.8,0.8,1.7,0.9,2.6H39c0.6,0,1,0.4,1,1s-0.4,1-1,1h-2.1c-0.1,0.9-0.4,1.8-0.9,2.6l1.4,1.4c0.4,0.4,0.4,1,0,1.4 c-0.4,0.4-1,0.4-1.4,0l-1.3-1.3c-2.7,2.5-6.8,2.5-9.5,0L24,39.4c-0.4,0.4-1,0.4-1.4,0c-0.4-0.4-0.4-1,0-1.4l1.4-1.4 c-0.5-0.8-0.8-1.7-0.9-2.6H21c-0.6,0-1-0.4-1-1s0.4-1,1-1h2.1c0.1-0.9,0.4-1.8,0.9-2.6L22.6,28c-0.4-0.4-0.3-1.1,0.1-1.4	c0.4-0.3,0.9-0.3,1.3,0l1.3,1.3c0.2-0.2,0.5-0.4,0.8-0.6c-0.1-1.3,0.5-2.6,1.5-3.4c0,0,0-0.1,0-0.1c-0.2-1.2-1.3-1.9-2.4-1.7 c0,0-0.1,0-0.1,0c-0.6,0-1-0.4-1-1s0.4-1,1-1c1.6-0.1,3.1,0.6,3.9,2c0.2,0.3,0.3,0.7,0.4,1c0.2,0,0.4,0,0.6,0c0.2,0,0.4,0,0.7,0 C30.8,22.6,30.9,22.3,31.1,22z" />
+          </svg>
+        )}
       </SvgButton>
       <SvgButton
         id="btn_debug_run"
